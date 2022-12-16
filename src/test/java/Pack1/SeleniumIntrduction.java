e006ac2d7f2fbc43b682a54a1042e59a55110710
unknown <khushboorathore393@gmail.com>
2022-12-16 14:53:37 +0530

    first commit

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,16 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		public static void main(String[] args) {
+			//Invoking browser
+			//ChromeDriver ---> methods ---> get , close
+			//ChormeDriver driver = new ChromeDriver();
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Downloads\\chromedriver1\\chromedriver.exe");
+			
+			WebDriver  driver = new ChromeDriver();
+			driver.get("https://www.instagram.com/");
+		}
 }
