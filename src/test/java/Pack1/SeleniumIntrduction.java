48d1d2c097a94f66d057de349530a31c02821268
Belu jain <belujain.01@gmail.com>
2022-12-16 14:32:35 +0530

    firstcommit

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,9 +1,15 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// invoking browse
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\asus\\Downloads\\chromedriver1\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.w3schools.com/");
 
 	}
 
