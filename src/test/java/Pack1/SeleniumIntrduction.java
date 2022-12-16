3d9ffe44d73890fd61a462fb3135588c81e6df74
Imran Mohammad <mdim387@gmail.com>
2022-12-16 15:05:19 +0530

    firstcommit

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,22 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Invoking browser
+				//ChromeDriver --->methods--> get,close
+				
+				//ChromeDriver driver =new ChromeDriver();
+				//we can also write
+				System.setProperty("webdriver.chrome.driver", "C:\\Users\\mdim3\\OneDrive\\Desktop\\chromedriver_win32\\chromedriver.exe");
+				WebDriver driver=new ChromeDriver();
+				driver.get("https://www.w3schools.com/");
+			}
+		}
+	
 
-	}
 
-}
