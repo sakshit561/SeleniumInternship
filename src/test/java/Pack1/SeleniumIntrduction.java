e1e783db177a754527a7fd7cbc824b9e5ed26cb9
Aarav Saxena <120356339+SaxenaAarav@users.noreply.github.com>
2022-12-16 15:04:42 +0530

    firstcommit

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,21 @@
 package Pack1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//invoking Browser
+		//ChromeDriver - Method (get , close)
 
+		//ChromeDriver driver = new ChromeDriver();
+
+		//or we can create this way also
+		WebDriver driver =  new ChromeDriver();
+
+		System.setProperty("WebDriver.Chrome.driver", "C:\\Users\\dell\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		driver.get("https://www.w3schools.com/");
 	}
 
 }
