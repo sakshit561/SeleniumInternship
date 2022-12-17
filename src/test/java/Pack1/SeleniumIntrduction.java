eedfbce774c5b4826bb0f4de499fc8a095fd6f71
swalehakhan <swalehakhan999@gmail.com>
2022-12-17 15:03:15 +0530

    Web Driver Setup

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,17 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//ChromeDriver driver=new ChromeDriver();
+		System.setProperty("Webdriver.Chrome.Driver","C:\\Users\\asus\\Downloads\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.w3school.com/");
+		
 	}
 
 }
