008498971ec44f45a3d2c25c272973bfd1cf1dec
juli thakur <julithakur72@gmail.com>
2022-12-17 14:45:58 +0530

    new changes

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,34 @@
 package Pack1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//System.out.println("hello");
+		WebDriverManager.chromedriver().setup();
+		WebDriver  driver =new ChromeDriver();
+		driver.get("https://in.linkedin.com/");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("session_key")).sendKeys("7879577398");
+		driver.findElement(By.id("session_password")).sendKeys("abcdefgh");
+	
 
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
-
 }
