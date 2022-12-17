3a217dd6f96de2a7c129d1afe80fbf01f2c46bd6
Abhinav Agrawal <abhinavisone360@gmail.com>
2022-12-17 15:44:23 +0530

    first program

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -3,7 +3,7 @@ package Pack1;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumIntroduction {
+public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		
