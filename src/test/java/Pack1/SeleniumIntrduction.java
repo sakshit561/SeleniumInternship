f0d2309e32ab7a73ac82828a64743402ea2abd07
Manish Arya <manish180499@gmail.com>
2022-12-17 12:48:17 +0530

    Web Driver practice

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,9 +1,15 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver cd = new ChromeDriver();
+		System.setProperty("webdriver.crome.driver","C:\\Users\\Manish\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		cd.get("https://www.w3schools.com/html/");
 
 	}
 
---
+++ src/test/java/Pack1/seleniumwebdrivercommand.java
@@ -0,0 +1,24 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class seleniumwebdrivercommand {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.nopcommerce.com/en/demo");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"ph-topic\"]/div[2]/div/div[1]/div[2]/div/div/a[1]/span")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.id("Email")).sendKeys("hello");
+//		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("admin");
+//      driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button")).click();
+		
+ 
+	}
+
+}
