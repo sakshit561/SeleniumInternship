dc11976702ff4b687af5926f92b985d8b3c25e46
Abhinav Agrawal <abhinavisone360@gmail.com>
2022-12-17 15:05:49 +0530

    Web driver commands added

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,9 +1,17 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 WebDriver driver =  new ChromeDriver();
+			
+			System.setProperty("WebDriver.Chrome.driver", "C:\\Users\\asus\\Downloads\\ChromeDriver");
+			
+			driver.get("https://www.w3schools.com/");
 
 	}
 
