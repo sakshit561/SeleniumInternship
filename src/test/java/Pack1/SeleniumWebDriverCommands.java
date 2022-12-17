851cd35c3952ad72c928663b9e4caf529ac9c7d3
yashodhan <yashodhan189@gmail.com>
2022-12-17 14:03:10 +0530

    web Driver

---
+++ src/test/java/Pack1/SeleniumWebDriverCommands.java
@@ -0,0 +1,37 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumWebDriverCommands {
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Desktop\\chromedriver.exe");
+		//alternative method ..no use to write above line
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.nopcommerce.com/en/demo");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"ph-topic\"]/div[2]/div/div[1]/div[2]/div/div/a[1]/span")).click();
+		//driver.quit();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("admin@yourstore.com");
+		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("admin");
+		driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button")).click();
+	}
+}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
