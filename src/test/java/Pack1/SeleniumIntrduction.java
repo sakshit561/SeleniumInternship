4fb459702e45b5f327328804dda5536a03899730
chauhanstuti <120356441+chauhanstuti@users.noreply.github.com>
2022-12-17 15:06:11 +0530

    Update SeleniumIntrduction.java

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,11 +1,28 @@
 package Pack1;
 
-public class SeleniumIntrduction {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-	}
-
+public class seleniumwebdrivercommands {
+ public static void main (String[]args) {
+	 
+	 WebDriverManager.chromedriver().setup();
+	 WebDriver driver = new ChromeDriver();
+	 driver.get("https://www.nopcommerce.com/en/demo");
+	 driver.manage().window().maximize();
+	 driver.findElement(By.xpath("//*[@id=\"ph-topic\"]/div[2]/div/div[1]/div[2]/div/div/a[1]/span")).click();
+	 //System.out.println(driver.getTitle());
+	 driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("admin@yourstore.com");
+	 driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("admin");
+	 
+	 //driver.findElement("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button").click();
+	 
+	 //driver.quit();
+	 
+	 
+	 //driver.close();
+ }
 }
