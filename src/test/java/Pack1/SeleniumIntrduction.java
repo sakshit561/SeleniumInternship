f7052a01fc2dedccf531c843d46b15ed582f356a
ashishsharma.com <ashishsharmamorena3@gmail.com>
2022-12-17 15:24:23 +0530

    secondcommit

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,9 +1,15 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\amana\\Downloads\\chromedriver_win32\\chromedriver.exe" );
+WebDriver driver = new ChromeDriver();
+driver.get("https://www.adani.com/");
 
 	}
 
