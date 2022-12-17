6bbbad1244b649a29bb25b3d6bd86ddd09649b87
Charan Singh <mahorraj15@gmail.com>
2022-12-17 14:14:13 +0530

    secondproject

--- src/test/java/Pack1/GhanShyam.java
+++ src/test/java/Pack1/GhanShyam.java
@@ -3,7 +3,7 @@ package Pack1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+ 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GhanShyam {
---
+++ src/test/java/Pack1/Ram.java
@@ -0,0 +1,31 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+ 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Ram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		   WebDriver driver =  new ChromeDriver();
+		   //driver.get("https://www.nopcommerce.com/en/demo");
+		 //  driver.manage().window().maximize();
+		 //  driver.findElement(By.xpath("//*[@id=\"ph-topic\"]/div[2]/div/div[1]/div[2]/div/div/a[1]/span")).click();
+		//   System.out.println(driver.getTitle());
+		//   driver.findElement(By.xpath("//*[id=\"Email\"]")).sendKeys("admin@yourstore.com");
+		 //  driver.findElement(By.xpath("//*[id=\"Password\"]")).sendKeys("admin");
+		  // driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		   
+		 driver.get("https://www.saucedemo.com/");
+		 driver.manage().window().maximize();
+		 System.out.println(driver.getTitle());
+         driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+         driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+         driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+	}
+
+}
