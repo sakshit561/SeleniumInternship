e845efda947b7637b0b0453bde63688e99df317e
swalehakhan <swalehakhan999@gmail.com>
2022-12-17 16:01:52 +0530

    WebDriver setup

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,22 @@
 package Pack1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SeleniumIntrduction {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.nopcommerce.com/en/demo");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"ph-topic\"]/div[2]/div/div[1]/div[2]/div/div/a[1]/span")).click();
+		//driver.quit();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("admin@yourstore.com");
+		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("admin");
+		driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button")).click();
 	}
-
 }
