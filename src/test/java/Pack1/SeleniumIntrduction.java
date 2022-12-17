87151860e8f575b767eebf4605b505666a738c9e
Ajay <sjay7969@gmail.com>
2022-12-17 14:08:29 +0530

    Seleniumwebdriver

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,15 +1,20 @@
 package Pack1;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\amana\\Downloads\\chromedriver_win32\\chromedriver.exe" );
-		WebDriver driver = new ChromeDriver();
-driver.get("https://www.w3schools.com/");
-	}
+		//winvoking Browser
+		//ChromeDriver - Method (get , close)
 
+		//ChromeDriver driver = new ChromeDriver();
+
+		//or we can create this way also
+		WebDriver driver =  new ChromeDriver();
+
+		System.setProperty("WebDriver.Chrome.driver", "C:\\Users\\Dell\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+		driver.get("https://www.w3schools.com/");
+	}
 }
