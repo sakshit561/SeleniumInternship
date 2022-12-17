4a3e2b4b1eb49cf9e68c091a3273784ee83b407d
unknown <chauhanstuti144@gmail.com>
2022-12-17 14:55:22 +0530

    new change

--- src/test/java/Pack1/practise1.java
+++ src/test/java/Pack1/practise1.java
@@ -1,5 +1,18 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class practise1 {
+	public static void main(String []args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("www.google.com");
+	
+		
+		
+	}
 
 }
