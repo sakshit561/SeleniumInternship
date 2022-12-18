cc148d15d769fc29654c12bc9dd04dfd8f924bdf
yashodhan <yashodhan189@gmail.com>
2022-12-18 14:09:41 +0530

    secondcommit

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -1,10 +1,20 @@
 package Pack1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		        //invoking browser
+				//ChromeDriver--> methods --> get,close
+				//ChromeDriver driver=new ChromeDriver();
+				System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver.exe");
+					WebDriver driver= new ChromeDriver();
+					driver.get("http://www.adani.com/");
 
-	}
+			}
 
-}
+		}
+
+
+	
