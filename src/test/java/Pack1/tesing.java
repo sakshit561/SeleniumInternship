39d80cc006821389bf9a818c8c8cdc0ce9788b46
abhishek <rathorabhishek803@gmail.com>
2022-12-19 15:40:21 +0530

    first

---
+++ src/test/java/Pack1/tesing.java
@@ -0,0 +1,27 @@
+package Pack1;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class tesing {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ABC\\Downloads\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//*[@id=\"pass\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("7415730620");
+		driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("abhi@123");
+		driver.findElement(By.name("login")).click();
+
+	}
+
+}
