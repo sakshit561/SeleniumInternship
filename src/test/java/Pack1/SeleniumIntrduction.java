e846c0779e4c8f7df771f91b190e6802e6f7f12d
yash7049 <120355929+yash7049@users.noreply.github.com>
2022-12-19 23:59:39 +0530

    locators

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -5,25 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		        //invoking browser
-				//ChromeDriver--> methods --> get,close
-				//ChromeDriver driver=new ChromeDriver();
-				System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver.exe");
-					WebDriver driver= new ChromeDriver();
-					driver.get("http://www.adani.com/");
-=======
 		//invoking browser
 		//ChromeDriver--> methods --> get,close
 		//ChromeDriver driver=new ChromeDriver();
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Desktop\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver.exe");
 			WebDriver driver= new ChromeDriver();
 			driver.get("http://www.adani.com/");
->>>>>>> 851cd35c3952ad72c928663b9e4caf529ac9c7d3
 
-			}
+	}
 
-		}
-
-
-	
+}
--- src/test/java/Pack1/SeleniumWebDriverCommands.java
+++ src/test/java/Pack1/SeleniumWebDriverCommands.java
@@ -8,33 +8,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumWebDriverCommands {
 	public static void main(String[] args) {
-		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Desktop\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver.exe");
 		//alternative method ..no use to write above line
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.nopcommerce.com/en/demo");
 		driver.manage().window().maximize();
 		driver.findElement(By.xpath("//*[@id=\"ph-topic\"]/div[2]/div/div[1]/div[2]/div/div/a[1]/span")).click();
-		//driver.quit();
 		System.out.println(driver.getTitle());
 		driver.findElement(By.xpath("//*[@id=\"Email\"]")).sendKeys("admin@yourstore.com");
 		driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys("admin");
 		driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button")).click();
 	}
 }
-<<<<<<< HEAD
-=======
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-
->>>>>>> 851cd35c3952ad72c928663b9e4caf529ac9c7d3
