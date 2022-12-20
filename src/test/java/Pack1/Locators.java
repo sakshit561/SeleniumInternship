c964316ce123b6ce8cf151e1b923b5d5e0529ae7
Reshu Pradhan <reshupradhan8120@gmail.com>
2022-12-20 16:33:48 +0530

    my commit

--- src/test/java/Pack1/Locators.java
+++ src/test/java/Pack1/Locators.java
@@ -16,7 +16,7 @@ public class Locators {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\Downloads\\chromedriver\\chrome driver.exe");  
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("inputUsername")).sendKeys("Reshu pradhan");
+		driver.findElement(By.id("inputUsername")).sendKeys("abc");
 		driver.findElement(By.name("inputPassword")).sendKeys("123456");
 		driver.findElement(By.className("signInBtn")).click();
 		Thread.sleep(2000);
@@ -25,12 +25,16 @@ public class Locators {
 		
 		driver.findElement(By.id("inputUsername")).clear();
 		driver.findElement(By.linkText("Forgot your password?")).click();
-		driver.findElement(By.xpath("//input[@type='text'][1]")).sendKeys("Reshu");
+		driver.findElement(By.xpath("//input[@type='text'][1]")).sendKeys("abc");
 		driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("abc@gmail.com");
 		//driver.findElement(By.xpath("//input[@type='text'][3]")).sendKeys("7803068856");
 		driver.findElement(By.cssSelector("input[placeholder='Phone Number']")).sendKeys("7803068856");  //placeholder
 		
-		driver.findElement(By.xpath("//button[contains(@class,'reset-pwd-btn')]")).click();   
+		driver.findElement(By.xpath("//button[contains(@class,'reset-pwd-btn')]")).click(); 
+		//driver.findElement(By.xpath("//button[contains(@class,'go-to-login-btn')]")).click();
+		//driver.findElement(By.xpath("//input[@type='text'[2]")).sendKeys("abc");
+		//driver.findElement(By.id("inputUsername")).sendKeys("abc");
+		//driver.findElement(By.name("inputPassword")).sendKeys("123456");
 		//driver.close();
 		
 	}
