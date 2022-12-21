a23492d7392a9cfac787607ec4d8a61c6c908f42
Charan Singh <mahorraj15@gmail.com>
2022-12-21 15:44:43 +0530

    Dropdown

---
+++ src/test/java/Pack1/Dropdown1.java
@@ -0,0 +1,57 @@
+package Pack1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+public class Dropdown1 {
+
+
+		// TODO Auto-generated method stub
+		
+
+	
+
+	
+			public static void main(String[] args) throws InterruptedException {
+				// TODO Auto-generated method stub
+				System.out.println("Hello Charan");
+				System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+				String text="ind";
+				driver.manage().window().maximize();
+			    driver.get("https://www.rahulshettyacademy.com/dropdownsPractise/");
+			    
+			    // static dropdown
+			   
+			    WebElement el = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+			    Select dropdown =new Select(el);
+			    //dropdown.selectByIndex(2);
+			    dropdown.selectByVisibleText("USD");
+			    
+			    // Updated dropdown
+			    driver.findElement(By.id("divpaxinfo")).click();
+			    Thread.sleep(2000);
+			    for(int i=1;i<=5;i++) {
+			    driver.findElement(By.id("hrefIncAdt")).click();
+			    }
+			    driver.findElement(By.id("btnclosepaxoption")).click();
+			    
+			    // Dynamic dropdown
+			    
+			    driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+			    driver.findElement(By.xpath("//a[@value='GWL']")).click();
+			    Thread.sleep(2000);
+			    
+			    driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+			    driver.findElement(By.id("autosuggest")).sendKeys(text);
+			    Thread.sleep(2000);
+			    
+			    
+			    
+			}
+
+		
+	}
+
+
