dcc9aad6d6dc9166a7a973c16b79305ee4bf3a52
Reshu Pradhan <reshupradhan8120@gmail.com>
2022-12-22 14:14:58 +0530

    my fifth commit

--- src/test/java/Pack1/DropDown2.java
+++ src/test/java/Pack1/DropDown2.java
@@ -11,12 +11,14 @@ public class DropDown2 {
      
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\Downloads\\chromedriver\\chrome driver.exe");  
-		driver.get("https://www.spicejet.com/");
+		//driver.get("https://www.spicejet.com/");
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")));
 		
-		driver.findElement(By.xpath("(//input[@type='text'])[1]")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//div[@class='r-18u37iz'])[16]")).click();
-		driver.findElement(By.xpath("(//div[@class='css-1dbjc4n])[35]")).click();
+		//driver.findElement(By.xpath("(//input[@type='text'])[1]")).click();
+		//Thread.sleep(2000);
+		//driver.findElement(By.xpath("(//div[@class='r-18u37iz'])[16]")).click();
+		//driver.findElement(By.xpath("(//div[@class='css-1dbjc4n])[35]")).click();
 
 	}
 
