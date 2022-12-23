3909c1ecf3f54bf998d90c76e221b2b7ae9702e2
Aarav Saxena <120356339+SaxenaAarav@users.noreply.github.com>
2022-12-23 10:30:30 +0530

    Wait

--- src/test/java/Pack1/Wait.java
+++ src/test/java/Pack1/Wait.java
@@ -4,23 +4,34 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Wait {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\dell\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		//driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-		WebDriverWait w= new WebDriverWait(driver, null );
-		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+		driver.manage().window().maximize();
+
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		Thread.sleep(2000);
 		driver.findElement(By.xpath("//body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/button[1]")).click();
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
-		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("abc");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@class='promoCode']")).sendKeys("aarav");
 		driver.findElement(By.cssSelector("button[class='promoBtn']")).click();
-        
+		driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+		Thread.sleep(2000);
+		WebElement el = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/div/div/select"));
+		Select dropdown = new Select(el);
+		dropdown.selectByVisibleText("India");
+		driver.findElement(By.xpath("//input[@class='chkAgree']")).click();
+		driver.findElement(By.xpath("//button[text()='Proceed']")).click();
 	}
 
 }
