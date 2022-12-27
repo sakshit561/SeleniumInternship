e2a0eb5341a451eb4218758850bcc54bbd517d44
yash7049 <120355929+yash7049@users.noreply.github.com>
2022-12-27 12:07:01 +0530

    WindowHandlePractice

---
+++ src/test/java/Pack1/WindowHandlePractice.java
@@ -0,0 +1,50 @@
+package Pack1;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowHandlePractice{
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver d = new ChromeDriver();
+		WebDriverWait w= new WebDriverWait(d,Duration.ofSeconds(10));
+		d.get("https://www.rahulshettyacademy.com/loginpagePractise/");
+		d.manage().window().maximize();
+		d.findElement(By.cssSelector(".blinkingText")).click();
+		Set<String> window= d.getWindowHandles();
+		Iterator <String>it = window.iterator();
+		String parentId= it.next();
+		String childID=it.next();
+		d.switchTo().window(childID);
+		w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".im-para.red")));
+		System.out.println(d.findElement(By.cssSelector(".im-para.red")).getText());
+		String emailId =d.findElement(By.cssSelector(".im-para.red"))
+				.getText().split("at")[1].trim().split(" ")[0];
+		
+		System.out.println(emailId);
+		d.switchTo().window(parentId);
+		d.findElement(By.id("username")).sendKeys("rahulshettyacademy");
+		d.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("learning");
+		d.findElement(By.xpath("//*[@id=\"login-form\"]/div[4]/div/label[2]/span[2]")).click();
+		WebDriverWait w1 =new WebDriverWait(d, Duration.ofSeconds(3));
+		w1.until(ExpectedConditions.elementToBeClickable(By.id("okayBtn")));		
+		d.findElement(By.id("okayBtn")).click();
+		d.findElement(By.id("terms")).click();
+		d.findElement(By.cssSelector("input[name=signin]")).click();
+		
+		
+		
+		
+	}
+
+}
