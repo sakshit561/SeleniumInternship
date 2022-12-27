d0841f057b47f1d8530ef356577b899df407b2d5
manisharya <manish180499@gmail.com>
2022-12-27 16:52:52 +0530

    Frame

---
+++ src/test/java/Pack1/Frame.java
@@ -0,0 +1,42 @@
+package Pack1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Frame {
+public static void main(String[] args) {
+	System.out.println("Hello Manish Arya");
+	WebDriverManager.chromedriver().setup();
+	
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://jqueryui.com/droppable/");
+	System.out.println(driver.findElements(By.className("demo-frame")).size());
+	//driver.switchTo().frame(0);
+	
+	driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+	
+	driver.findElement(By.id("draggable")).click();
+	
+	Actions a=new Actions(driver);
+	
+	WebElement source=driver.findElement(By.id("draggable"));
+	WebElement target=driver.findElement(By.id("droppable"));
+	
+	a.dragAndDrop(source,target).build().perform();
+	//frame se bhar aane ke liye 
+	driver.switchTo().defaultContent();
+	
+	driver.findElement(By.linkText("Accept")).click();
+	
+	
+	
+	
+	
+}
+}
