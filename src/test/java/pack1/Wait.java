d6708c8d682278e994543c2fb5b11e026605cd94
ShubhamShri458 <shubhamshri458@gmail.com>
2022-12-27 16:43:59 +0530

    Wait

--- src/test/java/pack1/Wait.java
+++ src/test/java/pack1/Wait.java
@@ -1,12 +1,10 @@
 package pack1;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Wait {
 
