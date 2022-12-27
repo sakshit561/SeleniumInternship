b396c4634b6ff36790de786385669c8cd445483d
yash7049 <120355929+yash7049@users.noreply.github.com>
2022-12-27 14:31:06 +0530

    LinkCountPerform

---
+++ src/test/java/Pack1/LinkCountPerform.java
@@ -0,0 +1,24 @@
+package Pack1;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LinkCountPerform {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		WebElement FooterLink=driver.findElement(By.className("gf-BIG"));
+		System.out.println(FooterLink.findElements(By.tagName("a")).size());
+		
+		
+		
+	}
+
+}
