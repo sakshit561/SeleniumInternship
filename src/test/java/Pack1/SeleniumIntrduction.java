046d391f9c2bb3add2cd465bb2dd6c971ca46f59
Manish Arya <80216014+manish887100@users.noreply.github.com>
2022-12-28 10:29:54 +0530

    Update SeleniumIntrduction.java

--- src/test/java/Pack1/SeleniumIntrduction.java
+++ src/test/java/Pack1/SeleniumIntrduction.java
@@ -8,19 +8,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumIntrduction {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		        //invoking browser
+                                System.out.println("Hello Manish Arya");
 				//ChromeDriver--> methods --> get,close
 				//ChromeDriver driver=new ChromeDriver();
 				System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver.exe");
 					WebDriver driver= new ChromeDriver();
 					driver.get("http://www.adani.com/");
-=======
 		// TODO Auto-generated method stub
 		WebDriver cd = new ChromeDriver();
 		System.setProperty("webdriver.crome.driver","C:\\Users\\Manish\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		cd.get("https://www.w3schools.com/html/");
->>>>>>> ba8c84665c068bedc18c3f1fe5bca4181885c05c
 
 			}
 
