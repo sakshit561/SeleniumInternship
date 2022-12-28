0774991d17ae438f7374d1f8ccb091d9ab53ddd5
Nivedita Saxena <Nivedita9644@gmail.com>
2022-12-28 20:37:00 +0530

    linkcount

--- src/test/java/Pack1/LinkCountPerform.java
+++ src/test/java/Pack1/LinkCountPerform.java
@@ -16,12 +16,30 @@ public class LinkCountPerform {
 		System.out.println(driver.findElements(By.tagName("a")).size());
         WebElement footerlink = driver.findElement(By.id("gf-BIG"));
         System.out.println(footerlink.findElements(By.tagName("a")).size());
+        
         WebElement firstsection =driver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
         System.out.println(firstsection.findElements(By.tagName("a")).size());
-        
         for(int i=0;i<firstsection.findElements(By.tagName("a")).size();i++) {
         	String clicknewtab = Keys.chord(Keys.CONTROL,Keys.ENTER);
         	firstsection.findElements(By.tagName("a")).get(i).sendKeys(clicknewtab);
+        }
+        WebElement secondsection =driver.findElement(By.xpath("//table/tbody/tr/td[2]/ul"));
+        System.out.println(  secondsection.findElements(By.tagName("a")).size());
+        for(int i=0;i< secondsection.findElements(By.tagName("a")).size();i++) {
+        	String clicknewtab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+        	 secondsection.findElements(By.tagName("a")).get(i).sendKeys(clicknewtab);
+        }
+        WebElement thirdsection =driver.findElement(By.xpath("//table/tbody/tr/td[3]/ul"));
+        System.out.println( thirdsection.findElements(By.tagName("a")).size());
+        for(int i=0;i< thirdsection.findElements(By.tagName("a")).size();i++) {
+        	String clicknewtab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+        	 thirdsection.findElements(By.tagName("a")).get(i).sendKeys(clicknewtab);
+        }
+        WebElement fourthsection =driver.findElement(By.xpath("//table/tbody/tr/td[4]/ul"));
+        System.out.println(fourthsection.findElements(By.tagName("a")).size());
+        for(int i=0;i<fourthsection.findElements(By.tagName("a")).size();i++) {
+        	String clicknewtab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+        	fourthsection.findElements(By.tagName("a")).get(i).sendKeys(clicknewtab);
         }
         
         }
