93530913e0faf5b775e7ffe8e0d9a7eb16ffe2a4
ShubhamShri458 <shubhamshri458@gmail.com>
2022-12-29 14:44:52 +0530

    CertificationsDownloadPractice

---
+++ src/test/java/pack1/CertificationsDownloadPractice.java
@@ -0,0 +1,27 @@
+package pack1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+		public class CertificationsDownloadPractice {
+		public static void main(String[] args) {
+			
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions option=new ChromeOptions();
+		option.setAcceptInsecureCerts(true);
+
+		WebDriver driver=new ChromeDriver(option);
+		driver.manage().window().maximize();
+
+		driver.get("https://www.karvy.com/");
+		System.out.println(driver.getTitle());
+		
+
+
+	}
+	
+
+}
