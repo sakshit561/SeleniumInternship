30861d45ca530a142e25baa86053ff0c029c395c
Charan Singh <mahorraj15@gmail.com>
2022-12-29 13:56:31 +0530

    GetChildWindowTitle

---
+++ src/test/java/Pack1/GetChildWindowTitle.java
@@ -0,0 +1,47 @@
+package Pack1;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetChildWindowTitle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				// TODO Auto-generated method stub
+				System.out.println("Hello Charan");
+				System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+				driver.manage().window().maximize();
+		         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		         driver.findElements(By.tagName("a")).size();
+		         System.out.println( driver.findElements(By.tagName("a")).size());
+		     WebElement footerLink     = driver.findElement(By.id("gf-BIG"));
+		     System.out.println(footerLink.findElements(By.tagName("a")).size());
+		     WebElement firstSection=driver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+		     System.out.println(firstSection.findElements(By.tagName("a")).size());
+			  for (int i=0;i<firstSection.findElements(By.tagName("a")).size();i++) {
+				   String clicknewTab= Keys.chord(Keys.CONTROL,Keys.ENTER);
+				   firstSection.findElements(By.tagName("a")).get(i).sendKeys(clicknewTab);
+
+		      }
+			  Set<String> title=driver.getWindowHandles();
+			  Iterator<String>it = title.iterator();
+			  while(it.hasNext()) {
+				  driver.switchTo().window(it.next());
+				  System.out.println(driver.getTitle());
+				  
+				  
+			  }
+			}
+
+		
+
+	}
+
+
