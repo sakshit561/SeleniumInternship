f67a82b016c81b92d11b17331596cfd1eb9da67b
Imran Mohammad <mdim387@gmail.com>
2022-12-29 21:39:34 +0530

    Certifications_Download_Practice

---
+++ src/test/java/Pack1/CertificationsDownloadPractice.java
@@ -0,0 +1,25 @@
+package Pack1;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CertificationsDownloadPractice {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions option=new ChromeOptions();
+		option.setAcceptInsecureCerts(true);
+		
+		WebDriver d =  new ChromeDriver(option);
+		d.manage().window().maximize();
+		
+		d.get("https://www.karvy.com/");
+		System.out.println(d.getTitle());
+
+	}
+
+}
