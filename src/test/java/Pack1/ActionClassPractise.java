b17202152d3207e220c2d65a51bffc6a5cea765e
saurabhchaturvadi <chaturvadisaurabh@gmail.com>
2022-12-29 14:08:22 +0530

    ScreenShot

---
+++ src/test/java/Pack1/ActionClassPractise.java
@@ -0,0 +1,38 @@
+package Pack1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ActionClassPractise {
+
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\Abc\\Downloads\\chromedriver_win32\\chromedriver.exe");
+WebDriver d =  new ChromeDriver();
+d.manage().window().maximize();
+WebDriverWait w= new WebDriverWait(d,Duration.ofSeconds(10));
+
+d.get("https://www.amazon.in/");
+WebElement move=d.findElement(By.cssSelector("#nav-link-accountList"));
+Actions a=new Actions(d);
+a.moveToElement(move).perform();
+w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#twotabsearchtextbox")));
+//a.moveToElement(d.findElement(By.cssSelector("#twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("fan").build().perform();
+a.moveToElement(d.findElement(By.cssSelector("#twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("fan").keyDown(Keys.ENTER).build().perform();
+a.moveToElement(move).contextClick().perform();
+   
+
+}
+
+}
+
+
---
+++ src/test/java/Pack1/ScreenShot.java
@@ -0,0 +1,47 @@
+package Pack1;
+
+ 	
+
+
+
+import java.io.File;
+
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+
+import org.openqa.selenium.TakesScreenshot;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScreenShot {
+
+public static void main(String[] args) throws IOException {
+
+// TODO Auto-generated method stub
+
+
+
+System.setProperty("webdriver.chrome.driver","C:\\Users\\Abc\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+WebDriver driver = new ChromeDriver();
+
+driver.manage().window().maximize();
+
+driver.manage().deleteAllCookies();
+
+driver.get("https://www.google.com/");
+
+File scr=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+FileUtils.copyFile(scr, new File("C:\\Users\\Abc\\OneDrive\\Pictures\\Camera Roll\\Screenshot.png"));
+
+}
+
+}
+
+
