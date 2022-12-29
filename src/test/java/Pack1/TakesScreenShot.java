8747d4efaff17177c8af099a0c92f92ee74948bf
Imran Mohammad <mdim387@gmail.com>
2022-12-29 21:42:27 +0530

    Takes_Screenshot

---
+++ src/test/java/Pack1/TakesScreenShot.java
@@ -0,0 +1,27 @@
+package Pack1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TakesScreenShot {
+
+	public static void main(String[] args) throws IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com/");
+		File scr=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scr, new File("C:\\Users\\mdim3\\eclipse-workspace\\screenshot.png"));
+
+	}
+
+}
