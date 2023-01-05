7a1a59947106bdd98aa5ac94c528d2701e0ed9df
Aarav Saxena <120356339+SaxenaAarav@users.noreply.github.com>
2023-01-05 14:39:42 +0530

    Add files via upload

---
+++ TestNGautomation.java
@@ -0,0 +1,34 @@
+package Pack1;
+
+public class TestNGautomation{
+	static void  beforesuit() {
+		System.out.println("Before Suite");
+		}
+	static void  beforetest() {
+		System.out.println("Before Test");
+		}
+	static void  beforeclass() {
+		System.out.println("Before Class");
+		}
+	static void  beforemethod() {
+		System.out.println("Before Method");
+		}
+	static void  test() {
+		System.out.println("Test");
+		}
+	static void  aftermethod() {
+		System.out.println("After Method");
+		}
+	static void  afterclass() {
+		System.out.println("After Class");
+		}
+	static void  aftertest() {
+		System.out.println("After Test");
+		}
+	static void  aftersuite() {
+		System.out.println("After Suite");
+		}public TestNGautomation() {
+			
+
+}
+}
